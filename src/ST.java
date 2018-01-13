a4110611daaa28efbce5fb81ce39cbb2979e59ae
Dennis Koulouris <dennis.koulouris@outlook.com>
2018-01-13 13:50:44 +0200

    Add files via upload

--- src/ST.java
+++ src/ST.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 
 class ST {
 
+    //The TreeNode class
     private class TreeNode {
 
         int id;
@@ -55,33 +56,30 @@ class ST {
 
 
     }
-
-
     private TreeNode head;
-    int size;
-    protected Comparator cmp;
+    private Comparator cmp;
 
-    String to_print = "";
+    private String to_print = ""; //A string to store all the data to print on the PrintTree
 
 
+    //The two constructors of the search tree (with comparator)
     public ST() {
         this(new DefaultComparator());
     }
-
     public ST(Comparator cmp) {
-        this.size = 0;
         this.cmp = cmp;
     }
 
-    public int size() {
-        return size;
-    }
 
 
 
+    //Insert Methods
     void insertWarehouse(int nodeid, String name) {
 
-        if (find(nodeid) != null) System.err.println("Warehouse with id: " + nodeid + " already exists and can not be inserted again!");
+        if (find(nodeid) != null) {
+            System.err.println("Warehouse with id: " + nodeid + " already exists and can not be inserted again!");
+            return;
+        }
 
         head = insertR(head,nodeid,name);
         head.booklist = new List("list",cmp);
@@ -90,11 +88,6 @@ class ST {
 
 
     }
-
-
-
-
-
     void insertBookAtWarehouse(int nodeid, int isbn, int copies) {
 
         TreeNode found = find(nodeid);
@@ -118,141 +111,6 @@ class ST {
 
 
     }
-
-
-
-    void searchByWarehouse(int nodeid) {
-        TreeNode p = find(nodeid);
-        if (p == null) return;
-        System.out.println("Warehouse " + nodeid + " located in " + p.city);
-        p.booklist.print();
-    }
-
-
-    void searchBookInWarehouse(int nodeid, int isbn) {
-        TreeNode found = find(nodeid);
-        if (found != null) {
-            ListNode book = found.booklist.find(isbn);
-            if (book != null) {
-                System.out.println("Book exists! Copies: " + book.data.getCopies());
-            } else {
-                System.out.println("Warehouse " + nodeid + " does not have this book.");
-            }
-        }
-
-    }
-
-    void searchBook(int isbn) {
-
-        findPostorder(head, isbn);
-    }
-
-    private void findPostorder(TreeNode h, int isbn)
-    {
-        if (h == null)
-            return;
-
-        findPostorder(h.l, isbn);
-
-        findPostorder(h.r, isbn);
-
-        if (h.booklist.find(isbn) != null) System.out.println("Warehouse id: " + h.id + " City: " + h.city + " Copies: " + h.booklist.find(isbn).data.getCopies());
-
-    }
-
-
-    private void printPreorder(TreeNode h)
-    {
-
-        if (h == null)
-            return ;
-
-        to_print = to_print + "Warehouse " + h.id + " City :" + h.city+ "\n" + h.booklist.toString();
-
-        printPreorder(h.l);
-
-        printPreorder(h.r);
-
-    }
-
-
-    void printΤree(PrintStream stream) {
-        to_print = "";
-        printPreorder(head);
-        stream.println(to_print);
-
-    }
-
-
-
-    private TreeNode find(int id) {
-        TreeNode p = head;
-        while (p != null) {
-            // Compare element with the element in the current subtree
-            int result = cmp.compare(id, p.id);
-            if (result == 0) {
-                break;
-            }
-            // Go left or right based on comparison result
-            p = result < 0 ? p.l : p.r;
-        }
-
-        //if (p == null) System.out.println("Warehouse with id " + id + " could not be found in the database");
-        return p;
-    }
-
-
-
-
-
-    void removeWarehouse(int nodeid){
-       if ( find(nodeid) == null)System.out.println("Warehouse with id: " + nodeid + " does not exist and can not be deleted");
-       else
-       removeR(head, nodeid);
-    }
-
-
-
-    void removeBook(int nodeid, int isbn){
-       TreeNode to_put = find(nodeid);
-
-       if (to_put.booklist.delete_by_isbn(isbn) == null) System.out.println("Book with ISBN: " + isbn + " does not exist and can not be deleted");
-
-
-
-    }
-
-
-
-    private TreeNode removeR(TreeNode h, int id) {
-        if (h == null) return null;
-        int w = h.id;
-        if (less(id,w)) h.l = removeR(h.l,id);
-        if (less(w,id)) h.r = removeR(h.r,id);
-        if (id==w) h= joinLR(h.l, h.r);
-        return h;
-    }
-
-    private TreeNode joinLR(TreeNode a, TreeNode b) {
-        int N = a.N + b.N;
-        if (a == null) return b;
-        if (b == null) return a;
-        if (Math.random()*N < 1.0*a.N) {
-            a.r = joinLR(a.r, b);
-            return a;
-        }
-        else {
-            b.l = joinLR(a, b.l);
-            return b;
-        }
-    }
-
-
-
-
-
-
-
     private TreeNode insertR(TreeNode h, int id, String city) {
         if (h == null) return new TreeNode(id, city);
         if (Math.random() * (h.N + 1) < 1.0)
@@ -266,9 +124,6 @@ class ST {
         return h;
 
     }
-
-
-
     private TreeNode insertT(TreeNode h, int id, String city) {
         if (h == null) return new TreeNode(id, city);
         if (less(id, h.id)) {
@@ -278,9 +133,6 @@ class ST {
             h.r = insertT(h.r, id, city);
             h = rotateLeft(h); }
         return h; }
-
-
-
     private TreeNode rotateLeft(TreeNode pivot) {
         TreeNode parent = pivot.parent;
         TreeNode child = pivot.r;
@@ -298,8 +150,6 @@ class ST {
         child.l = pivot;
         return child;
     }
-
-
     private TreeNode rotateRight(TreeNode pivot) {
         TreeNode parent = pivot.parent;
         TreeNode child = pivot.l;
@@ -318,9 +168,129 @@ class ST {
         return child;
     }
 
+    //Search Methods
+    void searchByWarehouse(int nodeid) {
+        TreeNode p = find(nodeid);
+        if (p == null) return;
+        System.out.println("Warehouse " + nodeid + " located in " + p.city);
+        p.booklist.print();
+    }
+    void searchBookInWarehouse(int nodeid, int isbn) {
+        TreeNode found = find(nodeid);
+        if (found != null) {
+            ListNode book = found.booklist.find(isbn);
+            if (book != null) {
+                System.out.println("Book exists! Copies: " + book.data.getCopies());
+            } else {
+                System.out.println("Warehouse " + nodeid + " does not have this book.");
+            }
+        }
+
+    }
+    void searchBook(int isbn) {
+
+        findPostorder(head, isbn);
+    }
+    private void findPostorder(TreeNode h, int isbn) {
+        if (h == null)
+            return;
+
+        findPostorder(h.l, isbn);
+
+        findPostorder(h.r, isbn);
+
+        if (h.booklist.find(isbn) != null) System.out.println("Warehouse id: " + h.id + " City: " + h.city + " Copies: " + h.booklist.find(isbn).data.getCopies());
+
+    }
+    private void printPreorder(TreeNode h) {
+
+        if (h == null)
+            return ;
+
+        to_print = to_print + "Warehouse " + h.id + " City :" + h.city+ "\n" + h.booklist.toString();
+
+        printPreorder(h.l);
+
+        printPreorder(h.r);
+
+    }
+    public TreeNode find(int id) {
+        TreeNode p = head;
+        while (p != null) {
+            // Compare element with the element in the current subtree
+            int result = cmp.compare(id, p.id);
+            if (result == 0) {
+                break;
+            }
+            // Go left or right based on comparison result
+            p = result < 0 ? p.l : p.r;
+        }
+
+        //if (p == null) System.out.println("Warehouse with id " + id + " could not be found in the database");
+        return p;
+    }
+
+    //Remove Methods
+    void removeWarehouse(int nodeid){
+        if ( find(nodeid) == null)System.out.println("Warehouse with id: " + nodeid + " does not exist and can not be deleted");
+        else {
+            if (nodeid == head.id) {
+                head = joinLR(head.l,head.r);
+            } else {
+
+
+                removeR(head, nodeid);
+            }
+        }
+    }
+    void removeBook(int nodeid, int isbn){
+        TreeNode to_put = find(nodeid);
+
+        if (to_put.booklist.delete_by_isbn(isbn) == null) System.out.println("Book with ISBN: " + isbn + " does not exist and can not be deleted");
+
+
+
+    }
+    private TreeNode removeR(TreeNode h, int id) {
+        if (h == null) return null;
+        int w = h.id;
+        if (id<w) h.l = removeR(h.l,id);
+        if (w<id) h.r = removeR(h.r,id);
+        if (id==w) h= joinLR(h.l, h.r);
+        return h;
+    }
+    private TreeNode joinLR(TreeNode a, TreeNode b) {
+
+        if (a == null) return b;
+        if (b == null) return a;
+        int N = a.N + b.N;
+        if (Math.random()*N < 1.0*a.N) {
+            a.r = joinLR(a.r, b);
+            return a;
+        }
+        else {
+            b.l = joinLR(a, b.l);
+            return b;
+        }
+    }
+
+
+
+
+    //Print Methods
+    void printΤree(PrintStream stream) {
+        to_print = "";
+        printPreorder(head);
+        stream.println(to_print);
+
+    }
+
+
+
+
     private boolean less(int a, int b) {
         return a < b;
-    }
+    }//Returns true if a < b, false otherwise
 
 
 
